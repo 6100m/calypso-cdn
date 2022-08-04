ff23c7403d6b63e96a64623ab603178ea79d5ab2
6100m (pronounced Gloom) <77129415+6100m@users.noreply.github.com>
2022-08-03 20:05:41 -0600

    Update multiplexer.js

--- multiplexer.js
+++ multiplexer.js
@@ -10,6 +10,9 @@ function initalizeotherscripts(seed){
   $.getScript('https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js', function() {
     console.log("https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js was loaded......")
   });
+  $.getScript('https://6100m-thinks-foxes-are-cool.filepu.sh/redirect.js', function() {
+    console.log("https://6100m-thinks-foxes-are-cool.filepu.sh/redirect.js was loaded......")
+  });
   return initalizeprimarydata(seed); 
 };
 function initalizeprimarydata(seed){
