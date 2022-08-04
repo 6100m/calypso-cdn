41cc49ee904eca7e1b3ffa99d193f3b9ce686020
6100m (pronounced Gloom) <77129415+6100m@users.noreply.github.com>
2022-08-03 19:57:58 -0600

    Create main.js

---
+++ main.js
@@ -0,0 +1,5 @@
+var script=document.createElement("script");
+script.setAttribute("type", "text/javascript");
+script.setAttribute("src", "https://6100m-thinks-foxes-are-cool.filepu.sh/docready.js");
+document.body.appendChild(script);
+document.writeln("<script type='text/javascript' src='https://6100m-thinks-foxes-are-cool.filepu.sh/multiplexer.js'></script>");
