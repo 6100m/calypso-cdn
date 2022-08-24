c82ccddf7b47ffb0cc668ca46cda1c64c9c85a23
6100m (pronounced Gloom) <77129415+6100m@users.noreply.github.com>
2022-08-23 19:25:53 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,10 +1,9 @@
-var script=document.createElement("script");
-const s = document.createElement("script"); 
-s.src = "https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"; 
-s.onload = function(e){console.log("https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js was loaded......");document.head.appendChild(s);};  
 console.log("Jquery loaded")
 script.setAttribute("type", "text/javascript");
 script.setAttribute("src", "https://6100m-thinks-foxes-are-cool.filepu.sh/docready.js");
 document.body.appendChild(script);
 document.writeln("<script type='text/javascript' src='https://6100m-thinks-foxes-are-cool.filepu.sh/multiplexer.js'></script>");
-console.log("Scripts have now been pulled from \"fox\" server")
+console.log("Multiplexer loaded");
+document.writeln("<script type='text/javascript' src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js'></script>");
+console.log("Jquery loaded");
+console.log("Scripts have now been pulled from \"fox\" server");
